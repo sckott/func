53bfa14c138cf0cc3c66f88574f2e3ab5a4a190a
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-16 15:34:50 -0700

    rework func_load_path - use its own parsing internally to correctly eval from the temporary environmnet

--- R/func_load_path.R
+++ R/func_load_path.R
@@ -14,6 +14,16 @@
 func_load_path <- function(path, envir=globalenv(), eval=TRUE) {
   tempenv <- new.env()
   source(path, local=tempenv)
-  fxn <- get(ls(tempenv), envir=tempenv)
-  func_load(fxn, envir=envir, eval=eval)
+  fxns <- lapply(ls(tempenv), get, envir=tempenv)
+  invisible(lapply(fxns, each_fun_from_path, envir = envir, eval = eval,
+    env_from = tempenv))
+}
+
+each_fun_from_path <- function(x, envir, eval, env_from) {
+  argslist <- as.list(x, all=TRUE)
+  args <- pop(argslist)
+  for (i in seq_along(args)) {
+    z <- if (eval && is.call(args[[i]])) rlang::eval_tidy(args[[i]], env=env_from) else args[[i]]
+    assign(names(args[i]), z, envir=envir)
+  }
 }
